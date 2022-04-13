8636882382b97a31b44981bb0bb253c6dd7c3f4e
OKirsenko <senka3434@gmail.com>
2022-04-13 23:37:35 +0300

    upload

---
+++ src/js/modal.js
@@ -0,0 +1,40 @@
+const refs = {
+  closeBtn: document.querySelector('.js-modal-close'),
+  openBtn: document.querySelector('.js-modal-open'),
+  modal: document.querySelector('.js-modal'),
+  formEl: document.querySelector('.js-form'),
+};
+
+refs.openBtn.addEventListener('click', toggleModal);
+refs.closeBtn.addEventListener('click', toggleModal);
+refs.formEl.addEventListener('submit', onSubmit);
+
+const message = 'Поле обязательно для заполнения';
+
+function toggleModal() {
+  refs.formEl.reset();
+  refs.modal.classList.toggle('is-hidden');
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  const name = e.target.name;
+  const phone = e.target.phone;
+  const email = e.target.email;
+  console.log(phone.value);
+  if (name.value.trim().length === 0) {
+    name.classList.add('modal__input_error');
+    name.setAttribute('placeholder', message);
+    return;
+  } else if (phone.value.trim().length === 0) {
+    phone.classList.add('modal__input_error');
+    phone.setAttribute('placeholder', message);
+    return;
+  } else if (email.value.trim().length === 0) {
+    email.classList.add('modal__input_error');
+    email.setAttribute('placeholder', message);
+    return;
+  } else {
+    toggleModal();
+  }
+}
